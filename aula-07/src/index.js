4ad4092b1a11e15fdfe38535d76abe7eb578f08f
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-09-27 20:13:36 -0300

    Add swagger documentation

--- aula-07/src/index.js
+++ aula-07/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
 const customerRouter = require('./route/customer.route')
 const productRouter = require('./route/product.route')
+const swaggerFile = require('../swagger.json')
 
 const app = express()
 app.use(express.json())
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.get('/', (req, res) => {
   res.send('I am here!!!')
