d91d41ff446877976f3fcce00af4c8ac84a9336b
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-11-04 08:11:30 -0300

    Change configuration to use environment variable

--- aula-08/src/config/index.js
+++ aula-08/src/config/index.js
@@ -1,7 +1,14 @@
 module.exports = {
-  username: 'admin',
-  password: 'admin',
-  database: 'admin',
-  host: '127.0.0.1',
-  dialect: 'postgres'
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  dialect: 'postgres',
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 }
